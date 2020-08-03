4fab212947173a14b149134f6d5c394126c5ddb5
Muciej <kejdzuuu@gmail.com>
2020-08-03 23:45:18 +0200

    Ex. 2.9

--- phonebook/src/App.js
+++ phonebook/src/App.js
@@ -5,6 +5,7 @@ const App = (props) => {
   const [ persons, setPersons ] = useState(props.persons)
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
 
   const addName = (event) => {
     event.preventDefault()
@@ -32,9 +33,19 @@ const App = (props) => {
     setNewNumber(event.target.value)
   }
 
+  const handleFilterInput = (event) => {
+    setFilter(event.target.value)
+  }
+
+  const filteredPersons = persons.filter(person =>
+    person.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
       <h2>Phonebook</h2>
+      filter names <input onChange={handleFilterInput} />
+      <h2>Add a new person</h2>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameInput} />
@@ -48,7 +59,7 @@ const App = (props) => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person => 
+        {filteredPersons.map(person =>
           <Person key={person.id} person={person} />
         )}
       </ul>
